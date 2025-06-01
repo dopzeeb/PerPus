a9b2d7306bccf81a8bffd706d85d0096b0e4e127
Muhammad Syauqi <168622901+yukaiii19@users.noreply.github.com>
2025-06-01 16:02:39 +0700

    Create buku.java

---
+++ src/model/buku.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Buku {
+    private String judul;
+    private Integer id_buku;
+    private String penulis;
+    private Boolean isFiksi;
+    
+    public Buku() {
+        this.judul = "";
+        this.penulis = "";
+
+        this.isFiksi = false;
+    }
+    
+    public Buku(String judul, Integer id_buku, String penulis,Boolean isFiksi) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.isFiksi = isFiksi;
+    }
+    public String getJudul() {
+        return judul;
+    }
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+    public Integer getId_buku() {
+        return id_buku;
+    }
+    public void setId_buku(Integer id_buku) {
+        this.id_buku = id_buku;
+    }
+    public String getPenulis() {
+        return penulis;
+    }
+    public void setPenulis(String penulis) {
+        this.penulis = penulis;
+    }
+    public Boolean getIsFiksi() {
+        return isFiksi;
+    }
+    public void setIsFiksi(Boolean isFiksi) {
+        this.isFiksi = isFiksi;
+    }
+}
